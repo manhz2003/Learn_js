29823a8e51daaf9d3e217f484e0e3c280da20120
manh <manhthenguyen113@gmail.com>
2022-08-26 00:00:25 +0700

    changed file array.js

---
+++ ARRAY_JS/array.js
@@ -0,0 +1,28 @@
+// ARRAY JS
+// cú pháp tạo 1 mảng
+// có thể đưa vào trong mảng chuỗi, số, kiểu dữ liệu ...
+let arrName = [
+    'Mạnh Nguyễn',
+    'Hải Nguyễn',
+    15,
+    10,
+    null,
+    undefined,
+    {},
+    function() {}
+]
+console.log(arrName) // kiểm tra mảng
+console.log(arrName.length) // kiểm tra dộ dài mảng
+console.log(arrName[2]) // lấy ra phần tử mảng theo vị trí index, bắt đầu từ 0
+
+/* CÁC METHOD LÀM VIỆC VỚI ARRAY
+ 1. to string
+ 2. join
+ 3. pop
+ 4. push
+ 5. shift
+ 6. unshift
+ 7. splicing
+ 8. concat
+ 9. slicing
+ */
